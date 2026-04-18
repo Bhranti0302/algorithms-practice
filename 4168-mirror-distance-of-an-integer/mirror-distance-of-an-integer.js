836c1923b63ae468b36941bc6a23eeaeef4321ff
Bhranti Tamboli <110032274+Bhranti0302@users.noreply.github.com>
2026-04-18 22:01:14 +0530

    Time: 2 ms (16.03%) | Memory: 55.6 MB (60.31%) - LeetSync

---
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var mirrorDistance = function(n) {
+    
+    let reversed = parseInt(n.toString().split('').reverse().join(''));
+    
+    
+    return Math.abs(n - reversed);
+};
