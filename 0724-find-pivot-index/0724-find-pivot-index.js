41ccb691642c6f8b3c6230dede30275c0b38d49b
Bhranti Tamboli <110032274+Bhranti0302@users.noreply.github.com>
2026-04-22 20:51:29 +0530

    Time: 2 ms (69.26%), Space: 57.6 MB (59.99%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+   const totalSum = nums.reduce((a,b)=>a+b,0);
+   let leftSum = 0
+
+   for(let i=0; i<nums.length; i++){
+    let rigthSum = totalSum - leftSum - nums[i];
+     
+    if(leftSum === rigthSum){
+        return i;
+    }
+
+    leftSum += nums[i]
+   } 
+
+   return -1
+};
