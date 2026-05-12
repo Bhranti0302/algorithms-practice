df0bf293a5fecf96961fb576175776ad5f411c1b
Bhranti Tamboli <110032274+Bhranti0302@users.noreply.github.com>
2026-05-12 22:19:26 +0530

    Time: 5 ms (33.2%), Space: 56.7 MB (29.99%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = [];
+
+    let map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of s) {
+        // if closing bracket
+        if (char in map) {
+            let top = stack.pop();
+            
+            if (top !== map[char]) {
+                return false;
+            }
+        } else {
+            // opening bracket
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
+};
