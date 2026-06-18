9c286df8d301c39d5ee50996b534e5018a75dad2
Bhranti Tamboli <110032274+Bhranti0302@users.noreply.github.com>
2026-06-18 22:07:42 +0530

    Time: 0 ms (100%), Space: 53.6 MB (54.84%) - LeetHub

---
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} hour
+ * @param {number} minutes
+ * @return {number}
+ */
+var angleClock = function(hour, minutes) {
+    hour = hour % 12;
+
+    const hourAngle = (hour * 30) + (minutes * 0.5);
+    const minuteAngle = minutes * 6;
+
+    const diff = Math.abs(hourAngle - minuteAngle);
+
+    return Math.min(diff, 360 - diff);
+};
