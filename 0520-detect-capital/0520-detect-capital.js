5cd5f043cf9d53d4b9c2cb4c64cd4653293f20ef
Bhranti Tamboli <110032274+Bhranti0302@users.noreply.github.com>
2026-07-04 22:43:22 +0530

    Time: 1 ms (25.07%), Space: 54.9 MB (31.94%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function(word) {
+   let uppercase = 0;
+
+   for(let i=0; i<word.length; i++){
+    if(word[i] === word[i].toUpperCase()){
+        uppercase++;
+    }
+   } 
+
+   return uppercase === word.length || uppercase === 0 || (uppercase===1 && word[0] === word[0].toUpperCase())
+};
